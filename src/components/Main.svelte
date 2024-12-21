136344794bda8ce31bbaf16cff3c5cd1d6a222c0
Michael <mabramo11@yahoo.com>
2024-12-21 12:13:32 -0500

    added header typing effect

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -49,13 +49,18 @@
 	<section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
 		<div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
 			<h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-				Hi! I'm <span class="poppins text-violet-400">Mike </span> Abramo
-				<br />Full Stack
-				<span class="poppins text-violet-400">Developer</span>
+				Hi! I'm <br /><span class="poppins text-violet-400">Mike Abramo </span>
+				<br />& I'm a
+				<span class="poppins text-violet-400">
+					<img
+						src="https://readme-typing-svg.demolab.com?font=Poppins&weight=600&size=72&pause=100&color=A78BFA&vCenter=true&random=true&width=820&height=72&lines=Blockchain+Developer;Startup+Co-Founder;Crypto+Researcher;Open+Source+Maxi"
+						alt="Typing SVG"
+					/>
+				</span>
 			</h2>
 			<p class="text-base sm:text-lg md:text-xl">
 				My <span class="text-violet-400"> favorite tech</span> includes JavaScript (NEXT.JS or SvelteKit),
-				TailwindCSS, Node.js + Express.js & PostgreSQL or Firebase/Firestore!
+				TailwindCSS, ShadCN, Solidity & ThirdWeb or Moralis!
 			</p>
 			<button
 				class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
