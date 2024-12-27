5aea63115384a67626620499678116f4eb7e903d
Michael <mabramo11@yahoo.com>
2024-12-27 12:19:44 -0500

    updated tailwind config to match vercel's convention

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 // filepath: /home/sonnymonroe/Svelte-Projects/Svelte_Site/Mike_Abramo-Dev_Portfolio/tailwind.config.js
-module.exports = {
+export default {
 	purge: ['./src/**/*.{svelte,js,ts}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
